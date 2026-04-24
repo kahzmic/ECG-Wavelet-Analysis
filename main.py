a1fb8d92438983320525c134f8db4ce48db0ecb8
kahzmic <159095560+kahzmic@users.noreply.github.com>
2026-04-24 17:15:39 -0500

    main show things

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import wfdb
+
 def main():
-    print("Hello from final-project!")
+    # Download and read a WFDB record from PhysioNet
+    record = wfdb.rdrecord('104', pn_dir='mitdb')
+
+    # Plot the signal
+    wfdb.plot_wfdb(record=record, title='Record 100 from MIT-BIH Arrhythmia Database')
 
 
 if __name__ == "__main__":
